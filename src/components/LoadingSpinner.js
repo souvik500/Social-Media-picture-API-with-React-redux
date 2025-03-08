f67a1382c9d644187de3b12fe89ceba7ba210cf2
souvik500 <120314952+souvik500@users.noreply.github.com>
2025-03-08 19:01:07 +0000

    fg

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./styles/LoadingSpinner.css";
+import { ImSpinner3 } from "react-icons/im";
 
-const LoadingSpinner = () => <div className="spinner">Loading...</div>;
+const LoadingSpinner = () => (
+  <div className="spinner">
+    <ImSpinner3 />
+  </div>
+);
 
 export default LoadingSpinner;
